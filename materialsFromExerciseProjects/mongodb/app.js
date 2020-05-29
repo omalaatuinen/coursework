455b711e282ea3fad38ad6a30bcb1f0aacbc4c45
Alexey <aleksekokorev@gmail.com>
2020-05-29 16:51:39 +0300

    mongoose working well!

---
+++ materialsFromExerciseProjects/mongodb/app.js
@@ -0,0 +1,34 @@
+// Require the mongoose, that has been already installed through npm .
+
+const mongoose = require("mongoose");
+
+// connecting...
+
+mongoose.connect("mongodb://localhost:27017/testDB", {useNewUrlParser:true, useUnifiedTopology: true});
+
+// Creating a new chema 
+
+const visitorSchema = new mongoose.Schema({
+    taskList: Array
+});
+
+// Creating the mongoose model:
+
+const Visitor = mongoose.model("Visitors", visitorSchema);//the "Visitors" will be a name of the db's collection
+
+//Creating a new Document
+
+const visitor = new Visitor({
+    taskList: [{ //the document will consists of some tasks, that the webpage will produce for a visitor.
+        tName: "msgbox",
+        msg: "Text of a message on a page"
+    },{
+        tName:"delay",
+        delay: 1 //setting timeOut in seconds
+    },{
+        tName:"redirect",
+        url: "https://google.com"
+    }]
+});
+
+visitor.save(); //saving a "visitor" document in the "Visitors" collection of a db "testDB".
