69a0ce69ef795e728f601574e18687b2b108e5aa
Alexey <aleksekokorev@gmail.com>
2020-06-01 20:25:42 +0300

    footer, header and own date module added

--- materialsFromExerciseProjects/express/app.js
+++ materialsFromExerciseProjects/express/app.js
@@ -5,34 +5,40 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+const day = require(__dirname + "/date.js");
 let items = ['Buy Food', 'Cook Food', 'Eat Food'];
-
+let workItems = [];
 
 app.get("/", (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/html');
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options);
+    res.render('index', { listTitle: day, items: items });
 
+});
 
-    res.render('index', {lDay: day, items: items });
+app.get("/work", (req, res) => {
+    res.render("index", { listTitle: "Work", items: workItems });
+});
 
-
+app.get("/about", (req, res) => {
+    res.render("about", { listTitle: "About" });
 });
 
 app.post("/", (req, res) => {
-    if (req.body.newItem.length > 0) {
-        items.push(req.body.newItem);
-    };
-
-    res.redirect("/");
-
+    if (req.body.list == "Work") {
+        if (req.body.newItem.length > 0) {
+            workItems.push(req.body.newItem);
+        };
+        res.redirect("/work");
+    } else {
+        if (req.body.newItem.length > 0) {
+            items.push(req.body.newItem);
+        };
+        res.redirect("/");
+    }
 });
+
 
 
 
---
+++ materialsFromExerciseProjects/express/date.js
@@ -0,0 +1,8 @@
+const today = new Date();
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    const day = today.toLocaleDateString("en-US", options);
+    module.exports = day;
